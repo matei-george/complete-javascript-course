c58a3a4625968587f972b274145ac74ad220dc59
Mat(2.718)i <75086843+matei-george@users.noreply.github.com>
2023-03-07 11:16:50 +0200

    Add JS learning

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,21 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+console.log("jonas");
+console.log(23);
+
+let firstName = "Matt";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let _function = "Hello";
+console.log(_function);
+
+let PI = 3.141592;
+console.log(PI);
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+console.log(myFirstJob, myCurrentJob);
