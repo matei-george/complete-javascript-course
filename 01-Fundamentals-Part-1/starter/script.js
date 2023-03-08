5ffc6df65f3d9f6c200cd9239c4bf6d7d26fbef6
Mat(2.718)i <75086843+matei-george@users.noreply.github.com>
2023-03-08 15:13:02 +0200

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,21 +1,76 @@
-let js = "amazing";
-console.log(40 + 8 + 23 - 10);
-console.log("jonas");
-console.log(23);
+// [----Variables and numbers---]
 
-let firstName = "Matt";
+// let js = "amazing";
+// console.log(40 + 8 + 23 - 10);
+// console.log("jonas");
+// console.log(23);
 
-console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+// let firstName = "Matt";
 
-let _function = "Hello";
-console.log(_function);
+// console.log(firstName);
+// console.log(firstName);
+// console.log(firstName);
 
-let PI = 3.141592;
-console.log(PI);
+// let _function = "Hello";
+// console.log(_function);
 
-let myFirstJob = "Programmer";
-let myCurrentJob = "Teacher";
+// let PI = 3.141592;
+// console.log(PI);
 
-console.log(myFirstJob, myCurrentJob);
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+
+// console.log(myFirstJob, myCurrentJob);
+
+// [----Data Types----]
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// console.log(typeof javascriptIsFun);
+// console.log(typeof "Hello there");
+// console.log(typeof true);
+// console.log(typeof 3.141592);
+
+// javascriptIsFun = "YES";
+// console.log(javascriptIsFun);
+
+// let variable;
+// console.log(variable);
+// variable = null;
+// console.log(variable);
+
+// console.log(typeof null);
+
+// [----Let var and Const----]
+// let age = 22;
+// age = 50;
+// const year = 2023;
+// console.log(year);
+
+// var job = "Brogrammer";
+// console.log(job);
+
+// [----Operator----]
+const currentYear = 2022;
+const ageMatt = currentYear - 1922;
+const ageSarah = currentYear - 1958;
+console.log(ageMatt, ageSarah, "<- Their age");
+
+const firstName = "Matei";
+const lastName = "Murarasu";
+console.log(firstName + " " + lastName);
+
+let x = 10 + 32;
+x += 10;
+x++;
+console.log(x);
+
+const isRightAge = ageSarah >= 18;
+
+console.log(currentYear - 1922 >= currentYear - 2018);
+
+let a, y;
+a = y = 2 ** 3;
+console.log(a, y);
+const avgAge = (ageMatt + ageSarah) / 2;
+console.log(ageMatt, ageSarah, avgAge);
