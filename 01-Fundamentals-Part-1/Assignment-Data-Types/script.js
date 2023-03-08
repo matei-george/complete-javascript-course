815207fca9d2a31df091038532070615d91eeccf
Mat(2.718)i <75086843+matei-george@users.noreply.github.com>
2023-03-08 15:13:31 +0200

    Add Assignments Solutions

---
+++ 01-Fundamentals-Part-1/Assignment-Data-Types/script.js
@@ -0,0 +1,6 @@
+const country = "Romania";
+let population = 19000000;
+const continent = "Europe";
+const isIsland = false;
+const language = "romanian";
+console.log(isIsland, population, country, language);
