4662f21018b4bfe9f6b47aee0ed3477b23797a76
Mat(2.718)i <75086843+matei-george@users.noreply.github.com>
2023-03-09 16:53:04 +0200

    Add Lecture practice problems.

--- 01-Fundamentals-Part-1/Assignment-Data-Types/script.js
+++ 01-Fundamentals-Part-1/Assignment-Data-Types/script.js
@@ -1,6 +1,26 @@
+// Lecture : Data Types and let,const, var
 const country = "Romania";
 let population = 19000000;
 const continent = "Europe";
 const isIsland = false;
 const language = "romanian";
 console.log(isIsland, population, country, language);
+
+// Lecture : Basic Operators
+let halfPopulation = population / 2;
+console.log("The population split by half would be", halfPopulation);
+halfPopulation++;
+console.log(halfPopulation);
+const finlandPopulation = 6000000;
+console.log(population > finlandPopulation);
+const avgPopulation = 33000000;
+console.log(population > avgPopulation);
+const description =
+  country +
+  " is in " +
+  continent +
+  " ,and its " +
+  population +
+  " people speak " +
+  language;
+console.log(description);
