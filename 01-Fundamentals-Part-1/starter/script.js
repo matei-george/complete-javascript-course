2927df83efb24575720a76324a9c72cd3efa6cd7
Mat(2.718)i <75086843+matei-george@users.noreply.github.com>
2023-03-10 11:06:37 +0200

    Add Lecture examples

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -51,28 +51,61 @@
 // console.log(job);
 
 // [----Operator----]
-const currentYear = 2022;
-const ageMatt = currentYear - 1922;
-const ageSarah = currentYear - 1958;
-console.log(ageMatt, ageSarah, "<- Their age");
+// const currentYear = 2022;
+// const ageMatt = currentYear - 1922;
+// const ageSarah = currentYear - 1958;
+// console.log(ageMatt, ageSarah, "<- Their age");
 
-const firstName = "Matei";
-const lastName = "Murarasu";
-console.log(firstName + " " + lastName);
+// const firstName = "Matei";
+// const lastName = "Murarasu";
+// console.log(firstName + " " + lastName);
 
-let x = 10 + 32;
-x += 10;
-x++;
-console.log(x);
+// let x = 10 + 32;
+// x += 10;
+// x++;
+// console.log(x);
 
-const isRightAge = ageSarah >= 18;
+// const isRightAge = ageSarah >= 18;
 
-console.log(currentYear - 1922 >= currentYear - 2018);
+// console.log(currentYear - 1922 >= currentYear - 2018);
 
-let a, y;
-a = y = 2 ** 3;
-console.log(a, y);
-const avgAge = (ageMatt + ageSarah) / 2;
-console.log(ageMatt, ageSarah, avgAge);
+// let a, y;
+// a = y = 2 ** 3;
+// console.log(a, y);
+// const avgAge = (ageMatt + ageSarah) / 2;
+// console.log(ageMatt, ageSarah, avgAge);
 
-//
+// // [----Strings and templates----]
+// const someName = "Matt";
+// const job = "programmer";
+// const birthYear = 2001;
+
+// const matt = "I'm " + someName + ", a " + job + " who is " + (currentYear - birthYear) + " years old.";
+// console.log(matt);
+// const mattNew = `I'm ${firstName}, a ${job} from Romania who is ${currentYear - birthYear} years old.`;
+// console.log(mattNew);
+// console.log(`This is just a string`);
+
+// console.log(`This is a string
+// which has multiple lines
+// of code.`);
+
+// [----If Else----]
+const age = 14;
+
+if (age >= 18) console.log(`Student is old enough to drive 😊`);
+else
+  console.log(`Student is not old enough to drive 😥. 
+Needs grow ${18 - age} more years`);
+
+const birthYear = 1853;
+let century;
+if (birthYear <= 1900) {
+  century = 19;
+} else if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+
+console.log(century);
