322cc2524d3308eae92cb86a5f1fac128f5ba1d0
Mat(2.718)i <75086843+matei-george@users.noreply.github.com>
2023-03-24 18:38:44 +0200

    Add Lecture notes

--- 02-Fundamentals-Part-2/starter/arrays.js
+++ 02-Fundamentals-Part-2/starter/arrays.js
@@ -6,13 +6,13 @@ const friend2 = "Jack";
 const friend3 = "Steven";
 
 // Literal Syntax
-const friend = ["Michael", "Jack", "Steven"];
-console.log(friend);
+const friends = ["Michael", "Jack", "Steven"];
+console.log(friends);
 
 // Dynamic Syntax
 const years = new Array(2001, 2002, 2003);
 
-const matt = ["Matei", "Murarasu", 2023 - 2001, "student", friend];
+const matt = ["Matei", "Murarasu", 2023 - 2001, "student", friends];
 
 console.log(matt);
 
@@ -22,3 +22,24 @@ const birthYears = [1990, 1968, 2002, 2010, 2018];
 const ages = new Array(5);
 for (let i = 0; i < birthYears.length; i++) ages[i] = calcAge(birthYears[i]);
 console.log(ages);
+
+// Methods that add
+const newLength = friends.push("Jay");
+console.log(newLength);
+
+friends.unshift("john");
+console.log(friends);
+// Methods that remove
+console.log(friends.pop());
+console.log(friends);
+friends.shift();
+console.log(friends);
+
+console.log(friends.indexOf("Steven"));
+
+console.log(friends.includes("Jack") ? true : false);
+
+friends.shift("Michael");
+friends.push("Michael");
+if (friends.includes("Michael")) console.log(`Michael is here`);
+else console.log(`Michael was here`);
