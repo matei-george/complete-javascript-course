251588ce324dc85cc3583b07998450a9b952f33a
Mat(2.718)i <75086843+matei-george@users.noreply.github.com>
2023-03-24 18:38:53 +0200

    Add Lecture Solution

--- 02-Fundamentals-Part-2/Assignment-Arrays/script.js
+++ 02-Fundamentals-Part-2/Assignment-Arrays/script.js
@@ -8,3 +8,16 @@ populations.length === 4 ? console.log(`True`) : console.log(`False`);
 const percentages = [];
 for (let i = 0; i < populations.length; i++) percentages[i] = percentageOfWorld(populations[i]);
 console.log(`Percentages of the world are : ${percentages}`);
+
+// Lecture : Array Methods
+const countryNeighbours = ["Ucraina", "Moldova", "Ungaria", "Serbia", "Bulgaria"];
+console.log(countryNeighbours);
+countryNeighbours.push("Utopia");
+console.log(countryNeighbours);
+countryNeighbours.pop();
+console.log(countryNeighbours);
+if (!countryNeighbours.includes("Germany")) console.log("Probably not a central European country 😁");
+countryNeighbours[countryNeighbours.indexOf("Moldova")] = "Republica Moldova";
+console.log(countryNeighbours);
+countryNeighbours[countryNeighbours.indexOf("Bulgaria")] = `Romania's brother from another mother`;
+console.log(countryNeighbours);
